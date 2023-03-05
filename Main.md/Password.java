2bae8b0ab158d95faf00b8a0366f2cb793b73c53
Sadasiva20 <99778144+Sadasiva20@users.noreply.github.com>
2023-03-05 15:14:55 -0500

    Add files via upload

---
+++ Main.md/Password.java
@@ -0,0 +1,98 @@
+import java.security.SecureRandom;
+
+
+public class Password {
+	
+	 Password(){
+		 
+		 
+		 
+	 } 
+	 
+	 
+	 public static int numbers() {
+		 
+		 int  random_num =0;
+		 
+		 SecureRandom  random_number  = new SecureRandom();
+		 
+		
+		 for(int number=0; number <=  random_number.nextInt(0,9);number++) {
+			
+			  
+			 random_num = number;
+			 
+		 }
+		 
+		  
+		  return random_num;
+	 }
+	 
+	public static char symbols() {
+		
+		
+		 char symbols_name = '*';
+		 
+ 
+		
+		 SecureRandom  random_symbol  = new SecureRandom();
+		 
+		
+		 for(int symbols =0 ; symbols <=random_symbol.nextInt(35,38);  symbols++) {
+		    
+	         
+			  
+			 symbols_name =(char)symbols  ;
+			  
+		 }
+		 
+		
+		 
+		   return symbols_name ;
+	}
+	
+	
+	public static char  uppercase() {
+		
+		char upper_case = 'A';
+		
+		SecureRandom  random_letter  = new SecureRandom();
+		 
+		for(int upper=0;  upper < random_letter.nextInt(65,90);upper++) {
+		 
+		 
+			 
+			 upper_case = (char) upper;
+			 
+			 
+		
+			 
+		} 
+		 
+		 
+		 return upper_case;
+		
+	}
+	
+	public static char lowercase() {
+		
+		char lower_case = 'b';
+		
+		SecureRandom  random_letter  = new SecureRandom();
+		for( int lower =0;  lower< random_letter.nextInt(97,122); lower++) {
+	    
+			 
+			 lower_case = (char)lower;
+			 
+			
+		}
+		 
+		   return lower_case;
+		
+	}
+	
+	
+	    
+	 
+	
+}
